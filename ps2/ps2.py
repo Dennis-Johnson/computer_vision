8f93d55b435cfcd697e82ced9295bca907bd57e9
Dennis Johnson <densarjohn@gmail.com>
2020-10-02 19:05:22 +0530

    Modified disparity__SSD to consider only (MAX_OFFSET/2) pixel centers on both the left and right of the base_window instead of the whole row.

--- ps2/ps2.py
+++ ps2/ps2.py
@@ -9,16 +9,14 @@ from tqdm import trange
 
 def disparity_ssd(imgL, imgR):
 
-    WINDOW_WIDTH = 19  # Use a window of size WINDOW_WIDTH x WINDOW_WIDTH
-    MAX_OFFSET = 10   # Maximum number of neighbouring pixels to check
+    WINDOW_WIDTH = 9  # Use a window of size WINDOW_WIDTH x WINDOW_WIDTH
+    MAX_OFFSET = 30   # Maximum number of neighbouring pixels to check
 
     # Number of cols and rows on the flanks of the centre pixel
     half_kernel = (WINDOW_WIDTH - 1) // 2
 
     rows, cols = imgL.shape
     disparity_matrix = np.zeros((rows, cols))
-
-    scale_factor = 1.2
 
     # Using trange to log the completion percentage
     for i in trange(half_kernel, rows - half_kernel):
@@ -29,9 +27,17 @@ def disparity_ssd(imgL, imgR):
             base_window = imgL[i - half_kernel:i +
                                half_kernel, j - half_kernel:j + half_kernel]
 
+            # Limit the centers of the right_window to only MAX_OFFSET/2 pixels
+            # on the left and right of the base_window's center.
+            # Additional hard bounding between the image boundary
+            # which is in the range [half_kernel, cols-half_kernel)
+            window_center_left_bound = max(j - MAX_OFFSET // 2, half_kernel)
+            window_center_right_bound = min(
+                j + MAX_OFFSET // 2, cols - half_kernel)
+
             # Compute SSD between base_window of the left image and all windows
             # along the same line on the right image.
-            for k in range(half_kernel, cols - half_kernel):
+            for k in range(window_center_left_bound, window_center_right_bound):
                 right_window = imgR[i - half_kernel:i +
                                     half_kernel, k - half_kernel:k + half_kernel]
                 diff = base_window - right_window
@@ -46,21 +52,40 @@ def disparity_ssd(imgL, imgR):
 
 
 def main():
-    # read left and right images
-    L = cv2.imread(os.path.join('input', 'pair0-L.png'), 0)
-    R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0)
+    # Question 1 -----------------------------------------------------
 
-    D_L = disparity_ssd(L, R)
-    D_R = disparity_ssd(R, L)
+    L1 = cv2.imread(os.path.join('input', 'pair0-L.png'), 0)
+    R1 = cv2.imread(os.path.join('input', 'pair0-R.png'), 0)
+
+    # Compute disparity matrices for left to right and right to left
+    D_LR1 = disparity_ssd(L1, R1)
+    D_RL1 = disparity_ssd(R1, L1)
 
     # Scale original disparity matrices to highlight the differences
-    SCALE_FACTOR = 255 / np.max(np.abs(D_L))
+    SCALE_FACTOR = 255 / np.max(np.abs(D_LR1))
 
-    scaledLR = np.abs(D_L) * SCALE_FACTOR
-    scaledRL = np.abs(D_R) * SCALE_FACTOR
+    scaledLR1 = np.abs(D_LR1) * SCALE_FACTOR
+    scaledRL1 = np.abs(D_RL1) * SCALE_FACTOR
 
-    cv2.imwrite("./output/ps2-1-a-1.png", scaledLR)
-    cv2.imwrite("./output/ps2-1-a-2.png", scaledRL)
+    cv2.imwrite("./output/ps2-1-a-1.png", scaledLR1)
+    cv2.imwrite("./output/ps2-1-a-2.png", scaledRL1)
+
+    # Question 2 -------------------------------------------------------
+    """
+    # Load images as greyscale --> flag = 0
+    L2 = cv2.imread(os.path.join('input', 'pair1-L.png'), 0)
+    R2 = cv2.imread(os.path.join('input', 'pair1-R.png'), 0)
+
+    D_LR2 = disparity_ssd(L2, R2)
+    D_RL2 = disparity_ssd(R2, L2)
+
+    SCALE_FACTOR = 255 / np.max(np.abs(D_LR2))
+    scaledLR2 = np.abs(D_LR2) * SCALE_FACTOR
+    scaledRL2 = np.abs(D_RL2) * SCALE_FACTOR
+
+    cv2.imwrite("./output/ps2-1-a-1.png", scaledLR2)
+    cv2.imwrite("./output/ps2-1-a-2.png", scaledRL2)
+    """
 
 
 if __name__ == "__main__":
